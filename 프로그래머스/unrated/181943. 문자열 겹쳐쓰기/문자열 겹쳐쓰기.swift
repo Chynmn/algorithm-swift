db85f774b6ded693bf0e3b594f0103f894ff9f52
HyunMin Cho <100783766+Chynmn@users.noreply.github.com>
2023-11-14 03:08:39 +0900

    [level unrated] Title: 문자열 겹쳐쓰기, Time: 0.05 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.swift
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.swift
@@ -1,20 +1,5 @@
 import Foundation
 
-func solution(_ my_string:String, _ overwrite_string:String, _ s:Int) -> String {
-    var result = ""
-    
-    if(s != 0) {
-        let frontIdx = my_string.index(my_string.startIndex, offsetBy: Int(s-1))
-        result.append(String(my_string[...frontIdx]))
-    }
-    
-    result.append(overwrite_string)
-    let idx = s + overwrite_string.count - my_string.count
-    if(idx != 0){
-        let backIdx = my_string.index(my_string.endIndex, offsetBy: idx)
-        result.append(String(my_string[backIdx...]))
-    }
-
-    
-    return result
+func solution(_ my_string:String, _ overwrite_string:String, _ s:Int) -> String {  
+    return String(my_string.prefix(s)) + overwrite_string + String(my_string.suffix(my_string.count - (overwrite_string.count + s)))
 }
