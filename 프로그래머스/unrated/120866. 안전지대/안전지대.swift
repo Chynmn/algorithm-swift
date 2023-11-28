1dcbc5735a99a6760dedc2b5000e6d20afdd1971
HyunMin Cho <100783766+Chynmn@users.noreply.github.com>
2023-11-28 17:59:28 +0900

    [level unrated] Title: 안전지대, Time: 0.12 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120866. 안전지대/안전지대.swift
+++ 프로그래머스/unrated/120866. 안전지대/안전지대.swift
@@ -1,20 +1,5 @@
 import Foundation
 
 func solution(_ board:[[Int]]) -> Int {
-
-    var answer: [[Int]] = Array(repeating: Array(repeating: 0, count: board.count), count: board.count)
-    
-    for (i, first) in board.enumerated() {
-        for (j, second) in first.enumerated() {
-            if board[i][j] == 1 {
-                for x in i-1...i+1 {
-                    for y in j-1...j+1 {
-                        if 0<=x && 0<=y && x<board.count && y<board.count { answer[x][y] = 1}
-                    }
-                }
-            }
-        }
-    }
-    
-    return board.count * board.count - answer.map( { $0.reduce(0, +)} ).reduce(0, +)
+    return 0
 }
