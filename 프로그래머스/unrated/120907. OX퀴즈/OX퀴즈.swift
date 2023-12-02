a0372e485782c9d7f34601682cf768df1df182ad
HyunMin Cho <100783766+Chynmn@users.noreply.github.com>
2023-12-02 15:19:47 +0900

    [level unrated] Title: OX퀴즈, Time: 0.13 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120907. OX퀴즈/OX퀴즈.swift
+++ 프로그래머스/unrated/120907. OX퀴즈/OX퀴즈.swift
@@ -3,21 +3,13 @@ import Foundation
 func solution(_ quiz:[String]) -> [String] {
     var answer: [String] = []
     
-    for i in quiz {
-        var list = i.split(separator: " ")
+    for q in quiz {
+        var list = q.components(separatedBy: " ")
         
         if String(list[1]) == "+" {
-            if Int(String(list[0]))! + Int(String(list[2]))! == Int(String(list[4]))! {
-                answer.append("O")
-            } else {
-                answer.append("X")
-            }
+            answer.append(Int(list[0])! + Int(list[2])! == Int(list[4])! ? "O" : "X")
         } else {
-            if Int(String(list[0]))! - Int(String(list[2]))! == Int(String(list[4]))! {
-                answer.append("O")
-            } else {
-                answer.append("X")
-            }
+            answer.append(Int(list[0])! - Int(list[2])! == Int(list[4])! ? "O" : "X")
         }
     }
     
